78b02798e6374726e44b3cffe70c422d0b6c56fa
HarshitGoyal27 <137126350+HarshitGoyal27@users.noreply.github.com>
2023-11-23 16:20:06 +0530

    Add files via upload

--- services/candidateService.js
+++ services/candidateService.js
@@ -2,24 +2,77 @@ const {
     successResponse,
     errorResponse,
 } = require('../utils/response/response.handler')
-import { getCandidatesZoho } from '../zohoDb/zohoCandidateApis';
-import { API_URL } from '../utils/constants/constants';
+const { getCandidatesZoho,getCandidateZoho,searchCandidateZoho,getFilteredZoho,getSortedCandidateZoho } =require('../zohoDb/zohoCandidateApis');
+const { API_URL } =require ('../utils/constants/constants');
 
-const buildSearchCriteria = (query) => {
+const buildSearchCriteria = (query) => { 
     const skillsCriteria = query.skills
       .map((skill) => `Skills:equals:${skill}`)
       .join("and");
     const designationCriteria = `Designation:equals:${query.designation}`;
     const allCriteria = [skillsCriteria, designationCriteria].join("and");
+    console.log(allCriteria)
     return encodeURIComponent(allCriteria);
-  }
+}
 
+const filterSearchcriteria=(query)=>{
+    const criteria = [];
+    if (query.experience){
+        criteria.push(`experience:between:${query.experience.min}:${query.experience.max}`);
+    }
+    if (query.location){
+        criteria.push(`location:equals:${query.location}`);
+    }
+    if (query.salaryType){
+        criteria.push(`salaryType:equals:${query.salaryType}`);
+    }
+    if (query.education){
+        const { schoolInstitution, degree, fieldOfStudy } = query.education;
+        if (schoolInstitution) criteria.push(`schoolInstitution:equals:${schoolInstitution}`);
+        if (degree) criteria.push(`degree:equals:${degree}`);
+        if (fieldOfStudy) criteria.push(`fieldOfStudy:equals:${fieldOfStudy}`);
+    }
+    if (query.availability){
+        const{noticePeriod,employmentType,immediateAvailability,onSite,relocate}=query.availability;
+        if (noticePeriod) criteria.push(`noticePeriod:equals:${noticePeriod}`);
+        if (employmentType) criteria.push(`employmentType:equals:${employmentType}`);
+        if (immediateAvailability) criteria.push(`immediateAvailability:equals:true`);
+        if (onSite) criteria.push(`onSite:equals:true`);
+        if (relocate) criteria.push(`relocate:equals:true`);
+    }
+    if (query.industryDomain){
+        criteria.push(`industryDomain:equals:${query.industryDomain}`);
+    }
+    if (query.certifications && query.certifications.length > 0){
+        const certificationsCriteria = query.certifications.map(cert => `certifications:equals:${cert}`);
+        criteria.push(...certificationsCriteria);
+    }
+    if (query.itSkills && query.itSkills.length > 0){
+        const itSkillsCriteria = query.itSkills.map(skill => `itSkills:equals:${skill}`);
+        criteria.push(...itSkillsCriteria);
+    }
+    if (query.diversity){
+        criteria.push(`diversity:equals:${query.diversity}`);
+    }
+    if (query.age){
+        criteria.push(`age:equals:${query.age}`);
+    }
+    if (query.lastActive){
+        criteria.push(`lastActive:equals:${query.lastActive}`);
+    }
+    const filterCriteria = criteria.join("and");
+    return encodeURIComponent(filterCriteria);
+}
+
+//this api hit
 const getCandidatesData = async (req) => {
     try {
         const searchQuery = req.body;
         const criteria = buildSearchCriteria(searchQuery);
         const url = `${API_URL}?criteria=${criteria}`;
-        const candidates = await getCandidatesZoho(url);
+        console.log(url);
+        console.log('Reached here!');
+        const candidates = await getCandidatesZoho(url);//function ending with zoho would make API calls
         return successResponse ({res, data: { candidates }, message: 'Success'})
     } catch (error) {
         return errorResponse ({res, error})
@@ -28,8 +81,11 @@ const getCandidatesData = async (req) => {
 
 const getCandidateData = async(req, res) => {
     try {
-        const candidates = await candidateService.getCandidateZoho(req);
-        return successResponse ({res, data: { candidates }, message: 'Success'})
+        let id=req.body.id;
+        const url=`${API_URL}?criteria=id:equals:${id}`;
+        console.log(url);
+        // const candidates = await candidateService.getCandidateZoho(url);
+        // return successResponse ({res, data: { candidates }, message: 'Success'})//function ending with zoho would make API calls
     } catch (error) {
         return errorResponse ({res, error})
     }
@@ -38,7 +94,7 @@ const getCandidateData = async(req, res) => {
 const searchCandidateData = async(req, res) => {
     try {
         const candidates = await candidateService.searchCandidateZoho(req);
-        return successResponse ({res, data: { candidates }, message: 'Success'})
+        return successResponse ({res, data: { candidates }, message: 'Success'})//function ending with zoho would make API calls
     } catch (error) {
         return errorResponse ({res, error})
     }
@@ -46,8 +102,14 @@ const searchCandidateData = async(req, res) => {
 
 const getFilteredData = async(req, res) => {
     try {
-        const candidates = await candidateService.getFilteredZoho(req);
-        return successResponse ({res, data: { candidates }, message: 'Success'})
+        console.log('Reached Here 2');
+        const searchQuery=req.body;
+        console.log(searchQuery);
+        const criteria=filterSearchcriteria(searchQuery);
+        const url = `${API_URL}?criteria=${criteria}`;
+        console.log(url);
+        const candidates = await candidateService.getFilteredZoho(url);
+        return successResponse ({res, data: { candidates }, message: 'Success'})//function ending with zoho would make API calls
     } catch (error) {
         return errorResponse ({res, error})
     }
@@ -55,8 +117,11 @@ const getFilteredData = async(req, res) => {
 
 const getSortedCandidateData = async(req, res) => {
     try {
-        const candidates = await candidateService.getSortedCandidateZoho(req);
-        return successResponse ({res, data: { candidates }, message: 'Success'})
+        let data=req.body;
+        const url=`${API_URL}?sort_order_by=${data.sortOrder}&sortBy=${data.sortBy}`;
+        console.log(url);
+        // const candidates = await candidateService.getSortedCandidateZoho(req);
+        // return successResponse ({res, data: { candidates }, message: 'Success'})//function ending with zoho would make API calls
     } catch (error) {
         return errorResponse ({res, error})
     }
