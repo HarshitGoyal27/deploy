61d04541f50ef01ef42c07ae8632f76df341c838
HarshitGoyal27 <137126350+HarshitGoyal27@users.noreply.github.com>
2023-12-06 13:23:48 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
-app.get('/', (req, res) => {
-    res.send('Hello, Skills Capital!!');
-});
-
+const cors = require('cors');
+const router=require('./routes/candidates');
+app.use(cors());
+app.use('/api/',router);
+app.get('/',(req,resp)=>{
+    resp.send('Hi!Skills Capital');
+})
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
+
