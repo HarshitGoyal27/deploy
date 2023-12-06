11bf5a06567e3a3872a56c09ed3ceba3f651dce4
HarshitGoyal27 <137126350+HarshitGoyal27@users.noreply.github.com>
2023-12-06 13:25:11 +0530

    Add files via upload

--- utils/constants/constants.js
+++ utils/constants/constants.js
@@ -1,1 +1,3 @@
-export const API_URL = "https://recruit.zoho.com/recruit/v2/Candidates/search";
+const API_URL = "https://recruit.zoho.in/recruit/v2/Candidates/search";
+const API_DELETED_COUNT = "https://recruit.zoho.com/recruit/v2/Candidates/deleted";
+module.exports={API_URL,API_DELETED_COUNT}
