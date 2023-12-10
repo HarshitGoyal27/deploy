3b8548af19475623421133c78149dff829f56b5e
HarshitGoyal27 <137126350+HarshitGoyal27@users.noreply.github.com>
2023-12-10 19:04:16 +0530

    Add files via upload

--- zohoDb/zohoAuthToken.js
+++ zohoDb/zohoAuthToken.js
@@ -1,11 +1,12 @@
 const axios=require('axios');
 const express=require('express');
 const fs=require('fs');
-let clientId='1000.DNYK7GIFFIWJKM992S9NR2X3B497FZ';
-let clientSecret='b3175156f597aa63d1901fe2e200ddba151a40a3ec';
-let codes='1000.fd01c8db6d6dd229a899933a7f9feb36.6ee4dd840fa1a709b65748af39255b13';
-let redirectUri='https://skillscapital.io/';
-let refreshtoken='1000.0b7ddec1149a0fd76dda4677a6c5031a.9487a874e5ca3b95e6714b487a8e8161'
+const clientId='1000.DNYK7GIFFIWJKM992S9NR2X3B497FZ';
+const clientSecret='b3175156f597aa63d1901fe2e200ddba151a40a3ec';
+const codes='1000.fd01c8db6d6dd229a899933a7f9feb36.6ee4dd840fa1a709b65748af39255b13';
+const redirectUri='https://skillscapital.io/';
+const refreshtoken='1000.0b7ddec1149a0fd76dda4677a6c5031a.9487a874e5ca3b95e6714b487a8e8161';
+let accessToken='';
 const requestData={
     grant_type: 'authorization_code',
     client_id: clientId,
@@ -31,11 +32,21 @@ console.log('A');
 //         console.log('Errrooorrrr',err);
 //     }
 // })();
-let getAuthTokenusingReferesh=(async()=>{
+let getAuthTokenusingReferesh=async()=>{
     try{
         let resp=await axios.post(`https://accounts.zoho.in/oauth/v2/token?refresh_token=${refreshtoken}&client_id=${clientId}&client_secret=${clientSecret}&grant_type=refresh_token`);
-        console.log(resp);
+        console.log('zohoauthtoken file',resp.data.access_token);
+        accessToken=resp.data.access_token;
     }catch(err){
         console.log('errrorr',err);
     }
-})();
+}
+
+let getAccess=()=>{
+    return accessToken;
+}
+
+module.exports={accessToken,getAccess,getAuthTokenusingReferesh};
+
+//1000.4c8f1b8598333689d528094d1c8e49ee.f70e472af2fa94177dea1d1d020438a6
+
--- zohoDb/zohoCandidateApis.js
+++ zohoDb/zohoCandidateApis.js
@@ -3,58 +3,12 @@ const {
   errorResponse,
 } = require("../utils/response/response.handler");
 const axios =require("axios");
-const accessToken = '1000.eb1b24e0636216cb58211ebcccc8eb09.2c98af739514ff5ac2b6bfcbf9a02f0c'; // Replace with your actual access token
+let {accessToken,getAccess} =require('./zohoAuthToken'); // Replace with your actual access token
 const fs=require('fs');
-const criteria='(Current_Location:contains:noida)and(Salary:contains:37)and(Last_Name:contains:PANDEY)';//296 info:{per_page:200,page:1,more_records:true}
-//hitting this method:
-//Stack
-//no need for base case just put () to the whole criteria string
-
-// let functionToSortBrackets=async(criteria,url)=>{
-//   // const queryRegex = /\((Skill_Set|Current_Salary):(equals|contains):([a-zA-Z0-9\s]+)\)/g;
-//   // //const opregex=/\d(and|or)\d/g//direct api call for 1 level of brackets
-//   // const queries = criteria.match(queryRegex) || [];
-//   // console.log(queries);
-//   // let stack=[];
-//   // queries.forEach(async(ele)=>{
-//   //     console.log(ele);
-//   //     let apilink=`https://recruit.zoho.in/recruit/v2/Candidates/search?criteria=${encodeURIComponent(ele)}`;
-//   //     try{
-//   //       let response=await APIcall(apilink);//response is object response={data:[{},{},{},.....]}
-//   //       if(stack.length===0){stack.push(response);}
-//   //       else{
-//   //         let dataTocompareWith=stack.pop();//and
-//   //         let exec=queryRegex.exec(ele);
-//   //         console.log('exec:::',exec[1],exec[2],exec[3]);
-//   //         //compare now
-//   //         let arr=[...response.data];
-//   //         d
-//   //       }
-//   //     }catch(err){
-//   //       console.log(err);
-//   //     }
-//   // })
-//   APIcall(url);
-// }
-
-// let APIcall=async(url)=>{
-//   try {
-//     const candidates = await axios.get(url, {
-//       headers: {
-//         'Authorization': `Zoho-oauthtoken ${accessToken}`,
-//       },
-//       params:{
-//         per_page:200
-//       }
-//     });
-  
-//     console.log('AAAAAAAA12345567890',candidates.data);///candidates.data.data
-//     return candidates.data;
-//   } catch (error) {
-//     console.log(error);
-//     return 'error';
-//   }
-// }
+const newAccessToken=()=>{
+  accessToken=getAccess();
+  console.log('Token is ',accessToken);
+}
 let getRequiredFields=(C_data)=>{
   // let total_count=C_data.info.count;
   // console.log(total_count);
@@ -78,14 +32,10 @@ let getRequiredFields=(C_data)=>{
   })
   return arr;
 }
-const getCandidatesZoho = async (res,url) => {//completed//FORM
+const getCandidatesZoho = async (res,url) => {
+  newAccessToken();
   try {
-    console.log('ZOHO FN',url);//till here okay
-
-    //functionToSortBrackets(query,url)
-    //const urL = `https://recruit.zoho.in/recruit/v2/Candidates/search?criteria=${encodeURIComponent(query)}`;
-    //const url='https://recruit.zoho.in/recruit/v2/Candidates';//GET RECORDS
-    //const urL1=`https://recruit.zoho.in/recruit/v2/Candidates/actions/count?criteria=${encodeURIComponent(criteria)}`
+    console.log('ZOHO FN',url);
     const candidates = await axios.get(url, {
       headers: {
         'Authorization': `Zoho-oauthtoken ${accessToken}`,
@@ -95,8 +45,6 @@ const getCandidatesZoho = async (res,url) => {//completed//FORM
         page:1
       }
     });
-    console.log('AAAAAA');
-    console.log(candidates.data);
     if(candidates.data!=''){
       let candidatesData=getRequiredFields(candidates.data);
       console.log(candidatesData);
@@ -105,22 +53,27 @@ const getCandidatesZoho = async (res,url) => {//completed//FORM
       return successResponse({res,data:{candidatesData:'Data not present'},message:"Success"});
     }
   } catch (error) {
-    console.log('HELL00');
     return errorResponse({ res, error });
   }
 };
-////////////////////////////////////////////////////////////////////////////////////////////////////////////
-const getCandidateZoho = async (res,url) => {
+
+const getCandidateZoho = async (res,url1,url2) => {
+  newAccessToken();
   try {
-    console.log('A',url);
-    const candidates = await axios.get(url, {
+    console.log('A');
+    const candidates = await axios.get(url1, {
       headers: {
         Authorization: `Zoho-oauthtoken ${accessToken}`, 
       },
     });
-    console.log('ID',candidates.data);
+    const tabular=await axios.get(url2,{
+      headers:{
+        Authorization: `Zoho-oauthtoken ${accessToken}`,
+      }
+    })
     if(candidates.data!=''){
       let candidatesData=getRequiredFields(candidates.data);
+      candidatesData.push(tabular.data.data[0]);
       console.log(candidatesData);
       return successResponse({ res, data: { candidatesData }, message: "Success" });
     }else{
@@ -133,6 +86,7 @@ const getCandidateZoho = async (res,url) => {
 };
 
 const searchCandidateZoho=async(url)=>{
+  newAccessToken();
   try {
     const candidates = await axios.get(url, {
       headers: {
@@ -144,7 +98,9 @@ const searchCandidateZoho=async(url)=>{
     return errorResponse({ res, error });
   }
 }
+
 const getFilteredZoho=async(url)=>{
+  newAccessToken();
   try {
     const candidates = await axios.get(url, {
       headers: {
@@ -157,6 +113,7 @@ const getFilteredZoho=async(url)=>{
   }
 }
 const getSortedCandidateZoho=async(url)=>{
+  newAccessToken();
   try {
     const candidates = await axios.get(url, {
       headers: {
@@ -171,6 +128,7 @@ const getSortedCandidateZoho=async(url)=>{
 
 let addCandidatesZoho=async(res,data)=>{//completed
   console.log('Reavhed here',data);
+  newAccessToken();
   let zohoapi='https://recruit.zoho.in/recruit/v2/Candidates'
     try{
       console.log('ABC');
@@ -187,11 +145,11 @@ let addCandidatesZoho=async(res,data)=>{//completed
     }
 }
 
-const getCandidatesSearchBarZoho=async(res,search)=>{
+const getCandidatesSearchBarZoho=async(res,url)=>{
+  newAccessToken();
   try{
     console.log('C');
-    let searcH1=`(Skill_Set:starts_with:${search})`;
-    let resp1=await axios.get(`https://recruit.zoho.in/recruit/v2/Candidates/search?criteria=${encodeURIComponent(searcH1)}`,{
+    let resp1=await axios.get(url,{
         headers: {
             'Authorization': `Zoho-oauthtoken ${accessToken}`,
           },
@@ -200,35 +158,16 @@ const getCandidatesSearchBarZoho=async(res,search)=>{
             page:1
         }
     });
-    // let searcH2=`(Current_Role:starts_with:${search})`;
-    // let resp2=await axios.get(`https://recruit.zoho.in/recruit/v2/Candidates/search?criteria=${encodeURIComponent(searcH2)}`,{
-    //     headers: {
-    //         'Authorization': `Zoho-oauthtoken ${accessToken}`,
-    //       },
-    //     params:{
-    //         per_page:5,
-    //         page:1
-    //     }
-    // });
-    console.log(resp1.data,'hello');
-   // console.log(resp2.data,'helloabcdefg');
     let arr=resp1.data.data;
     let searchSkills=[];
-    //let arr2=resp2.data.data;
     let currentrole=[];
     if(arr===undefined){
       return successResponse({ res, data: searchSkills, message: "Success" });
     }
-    // if(arr2===undefined){
-    //   return successResponse({ res, data: currentrole, message: "Success" });
-    // }
     searchSkills=arr.map((ele)=>{
         let skill=ele.Skill_Set.match(/^[^,]*/);
         return skill[0];
     })
-    // currentrole=arr2.map((ele)=>{
-    //     return ele.Current_Role;
-    // })
     console.log([...searchSkills]);
     const uniqueSkills = [...new Set(searchSkills)];
     return successResponse({ res, data: uniqueSkills, message: "Success" });
@@ -239,12 +178,11 @@ const getCandidatesSearchBarZoho=async(res,search)=>{
   }
 }
 
-const getLocationSearchBarZoho=async(res,search)=>{
+const getLocationSearchBarZoho=async(res,url)=>{
+  newAccessToken();
   try{
-    console.log('C1234',search);
-    let searcH=`(Current_Location:starts_with:${search})`;
-    console.log(searcH);
-    let resp=await axios.get(`https://recruit.zoho.in/recruit/v2/Candidates/search?criteria=${encodeURIComponent(searcH)}`,{
+    console.log('C1234');
+    let resp=await axios.get(url,{
         headers: {
             'Authorization': `Zoho-oauthtoken ${accessToken}`,
           },
@@ -253,7 +191,6 @@ const getLocationSearchBarZoho=async(res,search)=>{
             page:1
         }
     });
-    console.log(resp.data,'hello');
     let arr=resp.data.data;
     let searchLocation=[];
     console.log('this array',arr);
@@ -269,7 +206,7 @@ const getLocationSearchBarZoho=async(res,search)=>{
     return successResponse({ res, data: uniqueLocations, message: "Success" });
   }
   catch(error){
-    console.log('Loc',err);
+    console.log('Loc',error);
     return errorResponse ({res, error})
   }
 }
@@ -279,6 +216,7 @@ const updateCandidatesZoho=async(res)=>{
 }
 
 const deletedCandidatesZoho=async(res,url)=>{
+  newAccessToken();
   try{
     console.log(url);
     let resp=await axios.get(url,{
@@ -287,7 +225,7 @@ const deletedCandidatesZoho=async(res,url)=>{
       }
     })
     console.log(resp.data);
-    return successResponse({ res, data: resp.data, message: "Success" });
+     return successResponse({ res, data: resp.data, message: "Success" });
   }catch(error){
     //console.log(error)
     return errorResponse ({res, error})
@@ -297,4 +235,3 @@ module.exports = {
   getCandidatesZoho,getCandidateZoho,searchCandidateZoho,getFilteredZoho,getSortedCandidateZoho,addCandidatesZoho,getCandidatesSearchBarZoho,getLocationSearchBarZoho,
   deletedCandidatesZoho
 };
-//route->controller->service->zoho API
