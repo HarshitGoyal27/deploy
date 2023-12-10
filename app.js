e579dd9c43fea0543bb71cf24ab88590d1a168ef
HarshitGoyal27 <137126350+HarshitGoyal27@users.noreply.github.com>
2023-12-10 19:05:11 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 3000;
 const cors = require('cors');
 const router=require('./routes/candidates');
+let {getAuthTokenusingReferesh}=require('./zohoDb/zohoAuthToken');
 app.use(cors());
 app.use('/api/',router);
 app.get('/',(req,resp)=>{
@@ -11,4 +12,6 @@ app.get('/',(req,resp)=>{
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
+getAuthTokenusingReferesh();
+setInterval(getAuthTokenusingReferesh,60000*60);
 
