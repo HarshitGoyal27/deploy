f83cacdd8c140151d46976dcc06e19d8f42e236d
HarshitGoyal27 <137126350+HarshitGoyal27@users.noreply.github.com>
2023-12-13 15:01:32 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -7,10 +7,9 @@ const cluster = require("cluster");
 const numCPUs = require("os").cpus().length;
 const port = process.env.PORT || 3000;
 const { getAuthTokenusingReferesh } = require("./zohoDb/zohoAuthToken");
-
 app.use(cors());
 app.use("/api/", router);
-
+console.log('called the app.js again');
 if (cluster.isMaster) {
   console.log(`Master ${process.pid} is running`);
 
@@ -33,13 +32,13 @@ if (cluster.isMaster) {
   cluster.on("disconnect", (worker) => {
     console.log(`The worker #${worker.id} has disconnected`);
   });
+  getAuthTokenusingReferesh();
+  
+  setInterval(getAuthTokenusingReferesh, 60000*59);
 } else {
   app.listen(port, () =>
     console.log(`Server up and running on ${process.env.NODE_ENV} environment with port ${port} !`)
   );
-
-  getAuthTokenusingReferesh();
-  setInterval(getAuthTokenusingReferesh, 60000 * 60);
-
   console.log(`Worker ${process.pid} started`);
+
 }
