e3338d4112706a586ecc95860dfcff1e5f69041b
HarshitGoyal27 <harshitgoyal2705@gmail.com>
2023-12-19 18:25:46 +0530

    change

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const app = express();
 const cors = require("cors");
 const router = require("./routes/candidates");
 const cluster = require("cluster");
+const { Worker } = require("worker_threads");
 const numCPUs = require("os").cpus().length;
 const port = process.env.PORT || 3000;
-const { getAuthTokenusingReferesh } = require("./zohoDb/zohoAuthToken");
-
 app.use(cors());
 app.use("/api/", router);
-
+let token='';
+console.log('called the app.js again');
 if (cluster.isMaster) {
   console.log(`Master ${process.pid} is running`);
 
@@ -33,13 +33,12 @@ if (cluster.isMaster) {
   cluster.on("disconnect", (worker) => {
     console.log(`The worker #${worker.id} has disconnected`);
   });
-} else {
+  
+} 
+else {
   app.listen(port, () =>
     console.log(`Server up and running on ${process.env.NODE_ENV} environment with port ${port} !`)
   );
-
-  getAuthTokenusingReferesh();
-  setInterval(getAuthTokenusingReferesh, 60000 * 60);
-
   console.log(`Worker ${process.pid} started`);
+
 }
--- utils/constants/constants.js
+++ utils/constants/constants.js
@@ -2,4 +2,5 @@ const API_URL_SEARCH = "https://recruit.zoho.in/recruit/v2/Candidates/search";
 const API_DELETED_COUNT =
   "https://recruit.zoho.in/recruit/v2/Candidates/deleted";
 const API_URL_GET = "https://recruit.zoho.in/recruit/v2/Candidates";
-module.exports = { API_URL_SEARCH, API_DELETED_COUNT, API_URL_GET };
+const API_URL_GET_TABULAR_OLD="https://recruit.zoho.in/recruit/private/json/Candidates/getTabularRecords";
+module.exports = { API_URL_SEARCH, API_DELETED_COUNT, API_URL_GET , API_URL_GET_TABULAR_OLD};
